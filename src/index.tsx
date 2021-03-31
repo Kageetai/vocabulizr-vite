3a3871cc736cea1fb2c5d0e6f7a5e2be0a75687f
Kageetai <ma@kageetai.net>
2021-03-31 23:18:32 +0200

    add google analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import ReactGA from 'react-ga';
 
 import App from './components/App';
 import './index.css';
 import store from './store';
-
 import './utils/array';
+
+ReactGA.initialize(import.meta.env.VITE_GA_ID);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
